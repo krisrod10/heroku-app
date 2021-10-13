66288f834a5e08376e3f7dcefdd20c6012aaa44d
krisrod10 <krisrodgers12@icloud.com>
2021-10-12 19:54:29 -0500

    heroku test

---
+++ index.js
@@ -0,0 +1,18 @@
+// this is a express app
+let express = require("express");
+let env = require("dotenv").config();
+let app = express();
+
+// enabling the application to be able to parse json bodies in post/put
+app.use(express.json());
+
+// i want to use a router
+let itemRoute = require("./router/item")
+app.use(itemRoute);
+
+
+let port = process.env.PORT;
+
+app.listen(port, function(){
+    console.log("Starting the app server on port", port)
+});
---
+++ router/item.js
@@ -0,0 +1,9 @@
+let express = require("express");
+
+let router = express.Router();
+
+let controller = require("./controller/item");
+
+//POST /record -body {word: "Test"}
+// this will add a item to our database
+router.post("./controller/item", controller.)
